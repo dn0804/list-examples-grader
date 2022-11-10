961e75f8c70e85c43e837c1c9c32a7488ffaf063
dn0804 <78126917+dn0804@users.noreply.github.com>
2022-11-10 11:51:16 -0800

    :(

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,21 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class TestListExamples {
- 
+public class TestListExamples implements StringChecker {
+@Test
+ public void testFilter() {
+  List<String> list = new ArrayList<String>(Arrays.asList("aaa", "eee", "mmm", "lll"));
+  StringChecker sc = new Checker();
+  assertArrayEquals(new String[] {"aaa", "eee", "mmm", "lll"}, ListExamples.filter(list, sc).toArray());
+ }
+
+@Test
+public void testMerge() {
+  List<String> list1 = new ArrayList<String>(Arrays.asList("apple", "coconut", "mango"));
+  List<String> list2 = new ArrayList<String>(Arrays.asList("durian", "dragonfruit"));
+  assertArrayEquals(new String[] {"apple", "coconut", "dragonfruit", "durian", "mango"}, ListExamples.merge(list1, list2).toArray());
+}
+
 }
